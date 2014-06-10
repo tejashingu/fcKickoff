58a321273443b5a3e20bd458a8d8f10cd34f0860
nimittheinquister <nimithingu@gmail.com>
2014-06-11 01:20:34 +0530

    Facebook Easy Fb
    Configured for Phonegap

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 var brazilKickOff = angular.module('brazilKickOff', ['brazilKickOff.controllers','ezfb','brazilKickOff.services']);
 brazilKickOff.config(function (ezfbProvider) {
-    /**
-     * Basic setup
-     *
-     * https://github.com/pc035860/angular-easyfb#configuration
-     */
     ezfbProvider.setInitParams({
-        appId: '261787254027306'
+        appId: '261787254027306',
+        nativeInterface: CDV.FB,
+        useCachedDialogs: false
     });
+
+    ezfbProvider.setLoadSDKFunction(function ($document, ezfbAsyncInit) {
+        $document.on('deviceready', function () {
+            ezfbAsyncInit();
+        });
+    });
+})
+
+brazilKickOff.run(function (ezfb) {
+    ezfb.init();
 });
